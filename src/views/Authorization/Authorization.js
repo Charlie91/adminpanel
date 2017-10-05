5ec73ac9ac1725acbfdbc76d5a322c9ff8950ce1
Алексей <gaidarzhi@iMac-admin.local>
2017-10-05 14:09:54 +0300

    0.1.0

---
+++ src/views/Authorization/Authorization.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Authorization extends Component {
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        Hello Authorization
+      </div>
+    )
+  }
+}
+
+export default Authorization;
